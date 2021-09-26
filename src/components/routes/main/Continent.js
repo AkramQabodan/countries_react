11c49e6f8c9c4edf0c2912bab7de2869eb1d32ff
Akram Qabodan <akramqabodan22@gmail.com>
2021-09-27 00:11:24 +0200

    add filter by region component

--- src/components/routes/main/Continent.js
+++ src/components/routes/main/Continent.js
@@ -1,7 +1,11 @@
-import React from "react";
+import { React } from "react";
 import classes from "./Continent.module.css";
-
 function Continent() {
-  return <div className={classes.Continent}></div>;
+  return (
+    <div className={classes.dropdown}>
+      <button className={classes.button}>Filter by Region</button>
+      <div className={classes.continents}>content</div>
+    </div>
+  );
 }
 export default Continent;
--- src/components/routes/main/Main.js
+++ src/components/routes/main/Main.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Continent from "./Continent";
 import classes from "./Main.module.css";
 import SearchBar from "./SearchBar";
 function Main() {
   return (
     <div className={classes.main}>
-      <SearchBar />
+      <div className={classes.tools}>
+        <SearchBar />
+        <Continent />
+      </div>
     </div>
   );
 }
