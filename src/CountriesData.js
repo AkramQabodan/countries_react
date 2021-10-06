a679806422ee710457a58073a2a9c5711baa0d66
Akram Qabodan <akramqabodan22@gmail.com>
2021-10-06 16:28:49 +0200

    handle UI in countries not Countriesdata

--- src/CountriesData.js
+++ src/CountriesData.js
@@ -1,34 +1,21 @@
 import React, { useState, createContext, useEffect } from "react";
-import Country from "./components/routes/main/Country";
 
 export const CountriesData = createContext();
 
 export const CountriesProvider = (props) => {
   const [countries, setCountries] = useState();
-  const [UI, setUI] = useState();
+
   useEffect(() => {
     async function getCountries() {
       const raw = await fetch("https://restcountries.com/v2/all");
       const data = await raw.json();
       setCountries(data);
-      setUI(
-        data.map((i) => (
-          <Country
-            flag={i.flags.svg}
-            name={i.name}
-            population={i.population}
-            region={i.region}
-            capital={i.capital}
-            key={Math.random()}
-          />
-        ))
-      );
     }
     getCountries();
   }, []);
 
   return (
-    <CountriesData.Provider value={{ countries, UI }}>
+    <CountriesData.Provider value={countries}>
       {props.children}
     </CountriesData.Provider>
   );
--- src/components/routes/main/Countries.js
+++ src/components/routes/main/Countries.js
@@ -1,8 +1,30 @@
-import { React, useContext } from "react";
+import { React, useContext, useEffect, useState } from "react";
 import classes from "./Countries.module.css";
 import { CountriesData } from "../../../CountriesData";
+import Country from "./Country";
 function Countries() {
+  const [UI, setUI] = useState();
   const data = useContext(CountriesData);
-  return <div className={classes.countriesContainer}>{data.UI}</div>;
+
+  useEffect(() => {
+    if (!data) {
+      return;
+    } else {
+      setUI(
+        data.map((i) => (
+          <Country
+            flag={i.flags.svg}
+            name={i.name}
+            population={i.population}
+            region={i.region}
+            capital={i.capital}
+            key={Math.random()}
+          />
+        ))
+      );
+    }
+  }, [data]);
+
+  return <div className={classes.countriesContainer}>{UI}</div>;
 }
 export default Countries;
